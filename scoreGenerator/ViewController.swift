cd75092b45090065a415b36d59c785c04e09de84
nobu-g <ueda-n127@hotmail.co.jp>
2018-01-19 19:03:15 +0900

    Initial Commit

---
+++ scoreGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scoreGenerator
+//
+//  Created by 植田暢大 on 2018/01/19.
+//  Copyright © 2018年 植田暢大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
