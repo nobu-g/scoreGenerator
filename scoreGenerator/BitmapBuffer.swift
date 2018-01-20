de86f6f1bdd5dedb2e6ff1212b29fd6c9e0aaa3f
nobu-g <ueda-n127@hotmail.co.jp>
2018-01-20 20:54:02 +0900

    任意座標のRGB値を得られるように

---
+++ scoreGenerator/BitmapBuffer.swift
@@ -0,0 +1,56 @@
+//
+//  BitmapBuffer.swift
+//  scoreGenerator
+//
+//  Created by 植田暢大 on 2018/01/20.
+//  Copyright © 2018年 植田暢大. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BitmapBuffer {
+    private var pixelData: Data
+    let width: Int
+    let height: Int
+    private let bytesPerRow: Int
+    private let bytesPerPixel = 4
+    
+    init?(uiImage: UIImage) {
+        guard
+            //CGImageが取得できること
+            let cgImage = uiImage.cgImage
+            else {return nil}
+        //自分が望む形式でCGContextを作成する
+        width = cgImage.width
+        height = cgImage.height
+        let colorSpace = cgImage.colorSpace!
+        bytesPerRow = bytesPerPixel * width
+        pixelData = Data(count: height * bytesPerRow)
+        let bitsPerComponent = 8
+        pixelData.withUnsafeMutableBytes {(rawData: UnsafeMutablePointer<UInt8>)->Void in
+            let context = CGContext(data: rawData,
+                                    width: width,
+                                    height: height,
+                                    bitsPerComponent: bitsPerComponent,
+                                    bytesPerRow: bytesPerRow,
+                                    space: colorSpace,
+                                    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
+            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        }
+    }
+    
+    func getColor(x: Int, y: Int) -> UIColor {
+        let pixelInfo = bytesPerRow * y + x * bytesPerPixel
+        let r = CGFloat(pixelData[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(pixelData[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(pixelData[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(pixelData[pixelInfo+3]) / CGFloat(255.0)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    subscript (x: Int, y: Int) -> UIColor {
+        return getColor(x: x, y: y)
+    }
+}
--- scoreGenerator/ViewController.swift
+++ scoreGenerator/ViewController.swift
@@ -7,19 +7,125 @@
 //
 
 import UIKit
+import AVFoundation
+import AssetsLibrary
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    var frame = 0
+    var generator: AVAssetImageGenerator!
+    var imageView = UIImageView()   // 静止画用のImageView
+    var timer: Timer!
+    
+
+    
+//    @objc func tapped(_ sender: UITapGestureRecognizer){
+//        while(frame <= 1000) {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0/60) {
+//                let interval = CMTimeMake(Int64(self.frame), 60)
+//                var actual = CMTimeMake(Int64(0), 60)
+//                // 動画の指定した時間での画像を得る(今回は動画の最後のキャプチャを撮る).
+//                let capturedImage = try! self.generator.copyCGImage(at: interval, actualTime: &actual)
+//                self.imageView.image = UIImage(cgImage: capturedImage)
+//
+//                self.frame += 1
+//
+//                print("タップ\(CMTimeGetSeconds(actual))")
+//            }
+//        }
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad")
+//        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(
+//            target: self,
+//            action: #selector(ViewController.tapped(_:)))
+//
+//        // デリゲートをセット
+//        tapGesture.delegate = self
+//
+//        self.view.addGestureRecognizer(tapGesture)
+        // パスからassetを生成.
+        let path = Bundle.main.path(forResource: "READY!!", ofType: "MP4")
+        let fileURL = URL(fileURLWithPath: path!)
+        let avAsset = AVURLAsset(url: fileURL, options: nil)
+        
+        // assetから画像をキャプチャーする為のジュネレーターを生成.
+        generator = AVAssetImageGenerator(asset: avAsset)
+        generator.maximumSize = self.view.frame.size
+        generator.requestedTimeToleranceAfter = kCMTimeZero
+        generator.requestedTimeToleranceBefore = kCMTimeZero
+    
+        // 静止画用のImageViewを生成.
+        imageView = UIImageView(frame: self.view.frame)
+        
+        // imageViewをviewに追加.
+        self.view.addSubview(imageView)
+  
+        
+        
+        // 線を引く
+        
+        // イメージ処理の開始
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 1.0)
+        // パスの初期化
+        let drawPath = UIBezierPath()
+        drawPath.move(to: CGPoint(x: 70, y: 100))
+        drawPath.addLine(to: CGPoint(x: 70, y: 700))
+        // 線の色
+        UIColor.yellow.setStroke()
+        // 線を描く
+        drawPath.stroke()
+        
+        // イメージコンテキストから UIImage を作る
+        let drawImage = UIGraphicsGetImageFromCurrentImageContext()
+        // イメージ処理の終了
+        UIGraphicsEndImageContext()
+        
+        // イメージビューに設定する
+        let drawView = UIImageView(image: drawImage)
+        // 画面に表示する
+        view.addSubview(drawView)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        timer = Timer.scheduledTimer(timeInterval: 1/60, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        timer.invalidate()
+    }
+    
+    @objc func update(tm: Timer) {
+        let interval = CMTimeMake(Int64(self.frame), 60)
+        var actual = CMTimeMake(Int64(0), 60)
+        // 動画の指定した時間での画像を得る
+        let capturedImage = try! self.generator.copyCGImage(at: interval, actualTime: &actual)
+        self.imageView.image = UIImage(cgImage: capturedImage)
+        
+        if let pixelBuffer = BitmapBuffer(uiImage: UIImage(cgImage: capturedImage)) {
+            let components = pixelBuffer[200, 300].cgColor.components!
+            let red = components[0] * 255
+            let green = components[1] * 255
+            let blue = components[2] * 255
+            
+            print("red: \(red)  green: \(green)  blue: \(blue)")
+        }
+        
+        
+        
+        
+        
+        self.frame += 1
+//        print("タップ\(CMTimeGetSeconds(actual))")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-
+   
+    
 }
 
